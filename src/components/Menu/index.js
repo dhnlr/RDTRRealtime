c1c951916ab6eb00785b93349d751f3074e62f4f
angga-rch <angga.rch@gmail.com>
2021-03-19 11:34:55 +0700

    add dashboard page

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -9,43 +9,32 @@ function Menu() {
       <ul className="nav">
         <li className="nav-item">
           <A className="nav-link" href="index.html">
-            <i className="icon-grid menu-icon" />
             <span className="menu-title">Dashboard</span>
           </A>
         </li>
         <li className="nav-item">
-          <A className="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-            <i className="icon-layout menu-icon" />
-            <span className="menu-title">UI Elements</span>
-            <i className="menu-arrow" />
+          <A className="nav-link" href="index.html">
+            <span className="menu-title">Manajemen Data</span>
           </A>
-          <div className="collapse" id="ui-basic">
-            <ul className="nav flex-column sub-menu">
-              <li className="nav-item">
-                {" "}
-                <A className="nav-link" href="pages/ui-features/buttons.html">
-                  Buttons
-                </A>
-              </li>
-              <li className="nav-item">
-                {" "}
-                <A className="nav-link" href="pages/ui-features/dropdowns.html">
-                  Dropdowns
-                </A>
-              </li>
-              <li className="nav-item">
-                {" "}
-                <A className="nav-link" href="pages/ui-features/typography.html">
-                  Typography
-                </A>
-              </li>
-            </ul>
-          </div>
         </li>
         <li className="nav-item">
-          <A className="nav-link" href="pages/documentation/documentation.html">
-            <i className="icon-paper menu-icon" />
-            <span className="menu-title">Documentation</span>
+          <A className="nav-link" href="index.html">
+            <span className="menu-title">Simulasi</span>
+          </A>
+        </li>
+        <li className="nav-item">
+          <A className="nav-link" href="index.html">
+            <span className="menu-title">Laporan</span>
+          </A>
+        </li>
+        <li className="nav-item">
+          <A className="nav-link" href="index.html">
+            <span className="menu-title">Administrasi</span>
+          </A>
+        </li>
+        <li className="nav-item">
+          <A className="nav-link" href="index.html">
+            <span className="menu-title">Bantuan</span>
           </A>
         </li>
       </ul>
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -11,42 +11,102 @@ function Dashboard() {
         <div className="main-panel">
           <div className="content-wrapper">
             <div className="row">
-              <div className="col-md-12 grid-margin">
-                <div className="row">
-                  <div className="col-12 col-xl-8 mb-4 mb-xl-0">
-                    <h3 className="font-weight-bold">Welcome Aamir</h3>
-                    <h6 className="font-weight-normal mb-0">
-                      All systems are running smoothly! You have <span className="text-primary">3 unread alerts!</span>
-                    </h6>
+              <div className="col-md-12 grid-margin stretch-card">
+                <div className="card tale-bg">
+                  <div className="card-people mt-auto" style={{ paddingTop: "0px" }}>
+                    <img src="images/dashboard/info.png" alt="info" />
                   </div>
-                  <div className="col-12 col-xl-4">
-                    <div className="justify-content-end d-flex">
-                      <div className="dropdown flex-md-grow-1 flex-xl-grow-0">
-                        <button
-                          className="btn btn-sm btn-light bg-white dropdown-toggle"
-                          type="button"
-                          id="dropdownMenuDate2"
-                          data-toggle="dropdown"
-                          aria-haspopup="true"
-                          aria-expanded="true"
-                        >
-                          <i className="mdi mdi-calendar" /> Today (10 Jan 2021)
-                        </button>
-                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
-                          <A className="dropdown-item" href="#">
-                            January - March
-                          </A>
-                          <A className="dropdown-item" href="#">
-                            March - June
-                          </A>
-                          <A className="dropdown-item" href="#">
-                            June - August
-                          </A>
-                          <A className="dropdown-item" href="#">
-                            August - November
-                          </A>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-12 grid-margin stretch-card">
+                <div className="card position-relative">
+                  <div className="card-body">
+                    <div id="detailedReports" className="carousel slide detailed-report-carousel position-static pt-2" data-ride="carousel">
+                      <div className="carousel-inner">
+                        <div className="carousel-item active">
+                          <div className="row">
+                            <div className="col-md-6 col-xl-6 d-flex flex-column justify-content-start">
+                              <div className="row">
+                                <div className="col-md-6 grid-margin stretch-card">
+                                  <div className="card tale-bg" style={{ height: "50%" }}>
+                                    <div className="card-people mt-auto" style={{ paddingTop: "0px" }}>
+                                      <img src="images/dashboard/mask-group7.png" alt="info" />
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="col-md-6 mt-3">
+                                  <h4>Membuat peta perencanaan yang telah ada</h4>
+                                  <br />
+                                  Menggunakan file dari perencanaan database kami
+                                </div>
+                              </div>
+                            </div>
+                            <div className="col-md-6 col-xl-6">
+                              <div className="row">
+                                <div className="col-md-6 grid-margin stretch-card">
+                                  <div className="card tale-bg" style={{ height: "50%" }}>
+                                    <div className="card-people mt-auto" style={{ paddingTop: "0px" }}>
+                                      <img src="images/dashboard/mask-group7.png" alt="info" />
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="col-md-6 mt-3">
+                                  <h4>Membuat peta perencanaan yang telah ada</h4>
+                                  <br />
+                                  Menggunakan file dari perencanaan database kami
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="carousel-item">
+                          <div className="row">
+                            <div className="col-md-6 col-xl-6 d-flex flex-column justify-content-start">
+                              <div className="row">
+                                <div className="col-md-6 grid-margin stretch-card">
+                                  <div className="card tale-bg" style={{ height: "50%" }}>
+                                    <div className="card-people mt-auto" style={{ paddingTop: "0px" }}>
+                                      <img src="images/dashboard/mask-group7.png" alt="info" />
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="col-md-6 mt-3">
+                                  <h4>Membuat peta perencanaan yang telah ada</h4>
+                                  <br />
+                                  Menggunakan file dari perencanaan database kami
+                                </div>
+                              </div>
+                            </div>
+                            <div className="col-md-6 col-xl-6">
+                              <div className="row">
+                                <div className="col-md-6 grid-margin stretch-card">
+                                  <div className="card tale-bg" style={{ height: "50%" }}>
+                                    <div className="card-people mt-auto" style={{ paddingTop: "0px" }}>
+                                      <img src="images/dashboard/mask-group7.png" alt="info" />
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="col-md-6 mt-3">
+                                  <h4>Membuat peta perencanaan yang telah ada</h4>
+                                  <br />
+                                  Menggunakan file dari perencanaan database kami
+                                </div>
+                              </div>
+                            </div>
+                          </div>
                         </div>
                       </div>
+                      <a className="carousel-control-prev" href="#detailedReports" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true" />
+                        <span className="sr-only">Previous</span>
+                      </a>
+                      <a className="carousel-control-next" href="#detailedReports" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true" />
+                        <span className="sr-only">Next</span>
+                      </a>
                     </div>
                   </div>
                 </div>
