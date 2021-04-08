78fe1ec96d4737e56a4e4c14bd0055a4e0ee72c3
dhnlr <dhani.534@gmail.com>
2021-04-08 12:20:01 +0700

    Change sidebar icons

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -9,37 +9,37 @@ function Menu(props) {
       <ul className="nav">
         <li className={`nav-item ${props.active === "dashboard" ? "active" : ""}`}>
           <Link className="nav-link" to="/dashboard">
-            <i className="icon-grid menu-icon"></i>
+            <i className="ti-dashboard menu-icon"></i>
             <span className="menu-title">Dashboard</span>
           </Link>
         </li>
         <li className={`nav-item ${props.active.indexOf("manajemendata") !== -1 ? "active" : ""}`}>
           <Link className="nav-link" to="/manajemendata">
-            <i className="icon-grid-2 menu-icon"></i>
+            <i className="ti-folder menu-icon"></i>
             <span className="menu-title">Manajemen Data</span>
           </Link>
         </li>
         <li className={`nav-item ${props.active.indexOf("simulasi") !== -1 ? "active" : ""}`}>
           <Link className="nav-link" to="/simulasi">
-            <i className="icon-columns menu-icon"></i>
+            <i className="ti-map-alt menu-icon"></i>
             <span className="menu-title">Simulasi</span>
           </Link>
         </li>
         <li className="nav-item">
           <A className="nav-link" href="index.html">
-            <i className="icon-bar-graph menu-icon"></i>
+            <i className="ti-write menu-icon"></i>
             <span className="menu-title">Laporan</span>
           </A>
         </li>
         <li className="nav-item">
           <A className="nav-link" href="index.html">
-            <i className="icon-head menu-icon"></i>
+            <i className="ti-user menu-icon"></i>
             <span className="menu-title">Administrasi</span>
           </A>
         </li>
         <li className="nav-item">
           <A className="nav-link" href="index.html">
-            <i className="icon-paper menu-icon"></i>
+            <i className="ti-help-alt menu-icon"></i>
             <span className="menu-title">Bantuan</span>
           </A>
         </li>
