87cf047226dc24e7f89a994b47cc1a03c038b88f
dhnlr <dhani.534@gmail.com>
2021-10-05 08:47:24 +0700

    Add custom layer list, add legend widget, update arcgis js api

--- src/Constants.js
+++ src/Constants.js
@@ -21,8 +21,8 @@ const dev = {
   url: {
     API_URL: "https://rdtr.onemap.id/backend",
     // API_URL: 'https://f8a97c6e-0b6f-427a-be5e-e6a968b89d0e.mock.pstmn.io',
-    // ARCGIS_URL: "https://localhost:8443/server/rest/services"
-    ARCGIS_URL: "https://rdtr.onemap.id/server/rest/services",
+    ARCGIS_URL: "https://localhost:8443/server/rest/services"
+    // ARCGIS_URL: "https://rdtr.onemap.id/server/rest/services",
     // ARCGIS_TEST_URL: "https://localhost:8443/server/rest/services",
   },
   editorConfiguration: {
